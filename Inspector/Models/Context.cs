56cc2876c5b20ed31962e935616273b93d9ec092
ALEX OLIVEIRA <alex.oliveira74@fatec.sp.gov.br>
2021-05-12 21:37:42 -0300

    Upload - Teste CRUD.

---
+++ Inspector/Models/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inspector.Models
+{
+    public class Context:DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+            InicializacaoDB.Initialize(this);
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+    }
+}
