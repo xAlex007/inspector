43de538780c589f859e2c8ee89649e1db9d54e62
Alex Afonso <82684257+xAlex007@users.noreply.github.com>
2021-06-14 13:54:09 -0300

    Ajustes página inicial

--- Inspector/Pages/home.aspx.cs
+++ Inspector/Pages/home.aspx.cs
@@ -16,16 +16,16 @@ public partial class Pages_home : System.Web.UI.Page
             manageMenu.Text = "<div class='col-9 col-sm-8 col-lg-5 col-xl-4 ms-lg-5 mt-5 mt-lg-0 menu-border' style='border-color: #4B84D8; background-color: rgba(255,255,255,0.5);'>";
             manageMenu.Text += "<div class='row g-5 row-cols-2 py-5'>";
             manageMenu.Text += "<div class='col d-flex justify-content-end'>";
-            manageMenu.Text += "<a title='Templates' href = 'templates.aspx' class='item-gerenciamento p-2'><img src = '../Src/img/template.png' style='height: 90px;'></a>";
+            manageMenu.Text += "<a title='Templates' href = 'templates.aspx' class='item-gerenciamento pt-3 px-3'><img src = '../Src/img/template.png' style='height: 70px;'></a>";
             manageMenu.Text += "</div>";
             manageMenu.Text += "<div class='col d-flex justify-content-start'>";
-            manageMenu.Text += "<a title='Planos de Inspeção' href = 'plans.aspx' class='item-gerenciamento p-2'><img src = '../Src/img/test-passed.png' style='height: 90px;'></a>";
+            manageMenu.Text += "<a title='Planos de Inspeção' href = 'plans.aspx' class='item-gerenciamento p-2'><img src = '../Src/img/test-passed.png' style='height: 85px;'></a>";
             manageMenu.Text += "</div>";
             manageMenu.Text += "<div class='col d-flex justify-content-end'>";
-            manageMenu.Text += "<a title='Relatórios' href = '#' class='item-gerenciamento p-2'><img src = '../Src/img/doughnut-chart.png' style='height: 90px'></a>";
+            manageMenu.Text += "<a title='Relatórios' href = '#' class='item-gerenciamento pt-25'><img src = '../Src/img/doughnut-chart.png' style='height: 80px;'></a>";
             manageMenu.Text += "</div>";
             manageMenu.Text += "<div class='col d-flex justify-content-start'>";
-            manageMenu.Text += "<a title='Databooks' href = '#' class='item-gerenciamento p-2'><img src = '../Src/img/book-stack.png' style='height: 86px; margin-left: 3px;' class='mt-1'></a>";
+            manageMenu.Text += "<a title='Databooks' href = '#' class='item-gerenciamento pt-3 px-29'><img src = '../Src/img/book-stack.png' style='height: 75px;'></a>";
             manageMenu.Text += "</div></div></div>";
         }
     }
